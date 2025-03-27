a4f132b6b5d8135c97c97fd1d98447af70e6216a
yufan <wangyufan22@mails.ucas.ac.cn>
2025-03-27 22:27:23 +0800

    Update vcf-to-fasta.sh

--- vcf-to-fasta.sh
+++ vcf-to-fasta.sh
@@ -8,6 +8,11 @@ bgzip my_vcf_file.vcf
 tabix my_vcf_file.vcf.gz
 
 conda activate base
-python /data01/wangyf/software/vcf2fasta/vcf2fasta.py -f /data01/wangyf/project2/CyprinusCarpio/15.pop/7.annovar/new/genome/genome.fa -v fabp3.exon1.vcf.gz -g fabp3.gff -e exon -o out
+python /data01/wangyf/software/vcf2fasta/vcf2fasta.py \
+-f /data01/wangyf/project2/CyprinusCarpio/15.pop/7.annovar/new/genome/genome.fa \
+-v fabp3.exon1.vcf.gz \
+-g fabp3.gff \
+-e exon \
+-o out
 
 ## v2f/functions.py 替换该脚本，晓琦大师改好的！
