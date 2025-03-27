75892b5381a9b4bbef498045342130702c3c0b9e
yufan <wangyufan22@mails.ucas.ac.cn>
2025-03-27 16:18:01 +0800

    Create vcf-to-fasta.sh

---
+++ vcf-to-fasta.sh
@@ -0,0 +1,11 @@
+# https://github.com/santiagosnchez/vcf2fasta
+
+# The reference must be indexed using:
+samtools faidx ref.fa
+
+# And the VCF file should be tabix indexed and compressed:
+bgzip my_vcf_file.vcf
+tabix my_vcf_file.vcf.gz
+
+conda activate base
+python /data01/wangyf/software/vcf2fasta.py -f /data01/wangyf/project2/CyprinusCarpio/15.pop/7.annovar/new/genome/genome.fa -v fabp3.exon1.vcf.gz -g fabp3.gff -e exon -o out
