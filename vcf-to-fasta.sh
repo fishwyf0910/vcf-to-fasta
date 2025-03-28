8dd8e6e23180a55034c266152451d966f3db8676
yufan <wangyufan22@mails.ucas.ac.cn>
2025-03-28 13:43:11 +0800

    Update vcf-to-fasta.sh

--- vcf-to-fasta.sh
+++ vcf-to-fasta.sh
@@ -16,4 +16,5 @@ python /data01/wangyf/software/vcf2fasta/vcf2fasta.py \
 -o out
 
 ## v2f/functions.py 替换该脚本，晓琦大师改好的！
-## 结果中的exon是从0开始排，output文件夹中其他文件可删除（其中无变异）
+## 结果中的 exon 是从0开始排，output文件夹中其他文件可删除（其中无变异）
+## 如果要提 intron 或 utr 区域的序列，在 gff 中增加对应的行，并修改第9列中的信息
