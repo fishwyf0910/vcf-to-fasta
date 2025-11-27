9096731451fe3c7cab7b540f14e375b76495c6df
yufan <wangyufan22@mails.ucas.ac.cn>
2025-11-27 22:14:38 +0800

    Update vcf-to-fasta.sh

--- vcf-to-fasta.sh
+++ vcf-to-fasta.sh
@@ -15,6 +15,5 @@ python /data01/wangyf/software/vcf2fasta/vcf2fasta.py \
 -e intron \
 -o out
 
-## v2f/functions.py 替换该脚本，晓琦大师改好的！
 ## 结果文件夹中的 exon 是从0开始排，output文件夹中其他文件可删除（其中无变异）
 ## 如果要提 intron 或 utr 区域的序列，在 gff 中增加对应的行，并修改第3列为intron，第4、5列为对应的坐标
