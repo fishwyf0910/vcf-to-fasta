580a8a8d45a7ba4704de0a5337baadeb9910234c
yufan <wangyufan22@mails.ucas.ac.cn>
2025-11-27 22:12:45 +0800

    Update vcf-to-fasta.sh

--- vcf-to-fasta.sh
+++ vcf-to-fasta.sh
@@ -10,11 +10,11 @@ tabix my_vcf_file.vcf.gz
 conda activate base
 python /data01/wangyf/software/vcf2fasta/vcf2fasta.py \
 -f /data01/wangyf/project2/CyprinusCarpio/15.pop/7.annovar/new/genome/genome.fa \
--v fabp3.exon1.vcf.gz \
+-v fabp3.intron1.vcf.gz \
 -g fabp3.gff \
--e exon \
+-e intron \
 -o out
 
 ## v2f/functions.py 替换该脚本，晓琦大师改好的！
-## 结果中的 exon 是从0开始排，output文件夹中其他文件可删除（其中无变异）
-## 如果要提 intron 或 utr 区域的序列，在 gff 中增加对应的行，并修改第9列中的信息
+## 结果文件夹中的 exon 是从0开始排，output文件夹中其他文件可删除（其中无变异）
+## 如果要提 intron 或 utr 区域的序列，在 gff 中增加对应的行，并修改第3列为intron，第4、5列为对应的坐标
